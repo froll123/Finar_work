1ea73071fb0503e9f34d3d9049a55eb69eaabbc6
froll_123@mail.ru <froll_123@mail.ru>
2023-01-30 10:14:58 +0300

    Added the final task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿string[] arr_1 = {"Hello", "2", "world", ":-)"};
+        string[] arr_2 = {"1234", "1567", "-2", "computer science"};
+        string[] arr_3 = {"Russia", "Denmark", "Kazan"};
+        
+        int max_word_size = 3;
+        
+        string[] ArrOfTheShortWords(string[] arr)
+        {
+            int length = arr.Length;          
+            string[] result = new string[length];
+            int count = 0;
+            
+            for (int i = 0; i < length; i++)
+            {
+                if(arr[i].Length <= max_word_size)
+                {
+                    result[count] = arr[i];
+                    count++;
+                }
+            }
+            Array.Resize(ref result, count);
+            
+            return result;
+        }
+        
+        void PrintArrayWord(string[] arr)
+        {
+            if(arr.Length == 0)
+            {
+                Console.WriteLine("Массив пуст");
+            }
+            else
+            {
+                Array.ForEach(arr, (str) => Console.Write($"{str} "));
+                Console.WriteLine();
+            }
+        }
+        
+        void PrintTaskWord(string[] arr)
+        {
+            Console.WriteLine("Исходный массив: ");
+            PrintArrayWord(arr);
+            string[] ArrayShort = ArrOfTheShortWords(arr);
+            Console.WriteLine($"Результирующий массив (слова меньше или равны {max_word_size} символам)");
+            PrintArrayWord(ArrayShort);
+            Console.WriteLine();
+        }
+        PrintTaskWord(arr_1);
+        PrintTaskWord(arr_2);
+        PrintTaskWord(arr_3);
+
